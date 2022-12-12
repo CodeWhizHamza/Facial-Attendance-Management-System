788ec2267b2e0a15fd1eed9c7580ae5393b4a0a0
M. Hamza <muhammadhamzam1486@gmail.com>
2022-12-12 18:21:09 +0500

    add git ignore and setup ahmed's starter files
    Former-commit-id: 849f7cd54921cbcd56afa1fde93745637d0464ae

--- attendance.py
+++ attendance.py
@@ -1,2 +1,12 @@
-def main(window):
-    print("hello from attendance.py")
+import tkinter as tk
+
+
+def main():
+    window = tk.Tk()
+    window.title("Start Attendance")
+    window.geometry('720x520')
+
+    tk.Label(master=window, text='For Ahmed',
+             font='Arial 20 roman normal').pack()
+
+    window.mainloop()
