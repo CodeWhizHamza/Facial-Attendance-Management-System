fbf854fb2bab2a9ecd7801479ba23083ebaf34f5
M. Hamza <muhammadhamzam1486@gmail.com>
2022-12-13 10:19:37 +0500

    Take image and et its encodings

--- add_student.py
+++ add_student.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
+import cv2 as cv
+import face_recognition
 
 
 def main():
@@ -7,9 +9,44 @@ def main():
     window.title("Add student")
     window.geometry('600x400')
 
+    imageEncodings = tk.Variable()
+
     def showMessage(text):
         messageStr.set(text)
         message.config(text=messageStr.get())
+
+    def takeImage():
+        capture = cv.VideoCapture(0)
+
+        if not capture:
+            showMessage("Cannot open web cam.")
+
+        ret, frame = capture.read()
+
+        if not ret:
+            showMessage("Cannot get frame from camera")
+
+        faces = face_recognition.face_locations(frame)
+
+        if len(faces) == 0:
+            showMessage("No face detected.")
+            return
+        else:
+            showMessage("")
+
+        if len(faces) > 1:
+            showMessage("Multiple faces detected.")
+            return
+        else:
+            showMessage("")
+
+        face = faces[0]
+        cv.rectangle(frame, (face[3], face[0]),
+                     (face[1], face[2]), (255, 255, 255), 1)
+        cv.imshow("Image", frame)
+
+        encodings = face_recognition.face_encodings(frame, [face])[0]
+        imageEncodings.set(encodings)
 
     def saveData():
         studentName = nameEntry.get()
@@ -40,7 +77,14 @@ def main():
             showMessage("Please enter a valid semester number (integer)")
             return
 
-        print(studentName, studentCmsID, studentSemester)
+        if not imageEncodings.get():
+            showMessage("Please provide an image.")
+            return
+        else:
+            showMessage("")
+
+        # Saving data...
+
         window.destroy()
 
     title = ttk.Label(
@@ -80,6 +124,16 @@ def main():
     s.configure("my.TButton", font="Helvetica 16 roman normal")
 
     # Printing take image button
+    imageFrames = ttk.Frame(entriesFrame)
+    imageFrames.grid(column=1, row=3)
+
+    takeImageButton = ttk.Button(
+        master=imageFrames, text="Take image", style="my.TButton", command=takeImage)
+    takeImageButton.grid(column=0, row=0)
+
+    confirmImageButton = ttk.Button(
+        master=imageFrames, text="Confirm Image", style="my.TButton")
+    confirmImageButton.grid(column=1, row=0, pady=10)
 
     # Printing Buttons
     buttonsFrame = ttk.Frame(window)
