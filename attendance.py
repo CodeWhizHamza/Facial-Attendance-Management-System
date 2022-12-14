5c596014327f6237b2eb0eb563bcf7b1db5899e6
Ahmed Mohiuddin <mohiuddinlive@gmail.com>
2022-12-14 19:34:35 +0500

    Added facial recognition and get cmsID for face

--- attendance.py
+++ attendance.py
@@ -6,6 +6,7 @@ import cv2 as cv                                                        #importe
 import face_recognition                                                 #imported face_recognition for recognizing images
 import numpy as np                                                      #imported numpy for math functions
 
+from PIL import Image, ImageTk                                          #
 from config import*                                                     #imported config to get file paths
 
 def main():
@@ -13,8 +14,14 @@ def main():
     window.title("Start Attendance")
     window.geometry('720x520')
 
-    tk.Label(master=window, text='ahmed is not changing the stuff',
-             font='Arial 20 roman normal').pack()
+    capture = cv.VideoCapture(1)                                        #Getting a video capture object for the camera
+    capture.set(cv.CAP_PROP_FRAME_WIDTH, 720)                           #Width and
+    capture.set(cv.CAP_PROP_FRAME_HEIGHT, 480)                          #Height of Camera
+
+    #tk.Label(master=window, text='ahmed is not changing the stuff',
+             #font='Arial 20 roman normal').pack()
+    videoCapture = tk.Label(window)
+    videoCapture.pack()
 
     def getCSV(filename):                                               #function for getting encodings from csv files
         with open(f"./{directoryName}/{filename}") as f:
@@ -31,9 +38,23 @@ def main():
         encodings[file[:-4]] = getCSV(file)                             #Saving encodings in Series with proper primary keys
 
     print(encodings)                                                    #printing encodings series
+    
+    def runCamera():
+        ret, frame = capture.read()                                     #Get Captured frame from Camera
 
-    tk.Label(master=window, text=getCSV("4221.csv"), font='Arial 20 roman normal').pack()
+        smallFrame = cv.resize(frame, (0, 0), fx=1.0, fy=1.0)
+        smallFrame = cv.cvtColor(smallFrame, cv.COLOR_BGR2RGB)        
 
+        capturedImage = Image.fromarray(smallFrame)                   # Capture the latest frame and transform to image
+  
+        photoImage = ImageTk.PhotoImage(image=capturedImage)          # Convert captured image to photoimage
+
+        videoCapture.photoImage = photoImage                          # Displaying photoimage in the label
+  
+        videoCapture.configure(image=photoImage)                       # Configure image in the label
+        window.after(1, runCamera)                          # Repeat the same process after every 10 milliseconds
+    
+    runCamera()
     window.mainloop()
 
 
