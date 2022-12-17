e0e47aa2a11182bc0e26c04c948dd9c17a980c92
M. Hamza <muhammadhamzam1486@gmail.com>
2022-12-17 15:44:47 +0500

    add customtkinter to studentReport

--- student_report.py
+++ student_report.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from tkinter import ttk
 import sqlite3
 import csv
+import customtkinter as ctk
 
 from config import *
 from helper import getAttendancePercentageFor, downloadReport, loadName
@@ -11,24 +12,29 @@ def studentReport(id):
     window = tk.Tk()
     window.title("Student Report")
     window.geometry('720x480')
+    window.iconbitmap('resources/logo.ico')
+    window.resizable(width=False, height=False)
+    window.focus()
 
     studentName = loadName(id)
-    heading = tk.Label(master=window, text=f"{studentName}'s Attendance Report",
-                       font='Arial 20 roman normal')
-    heading.pack(pady=18)
+    heading = ctk.CTkLabel(master=window, text=f"{studentName}'s Attendance Report",
+                           font=ctk.CTkFont('Arial', size=24))
+    heading.pack(pady=24)
     reportFrame = tk.Frame(master=window)
     reportFrame.pack()
 
+    font16 = ctk.CTkFont('Arial', size=16)
+
     for index, course in enumerate(list(courses)):
         attendancePercentage = f'{getAttendancePercentageFor(id, course):.2f}%'
-        courseName = tk.Label(master=reportFrame, text=f"{course}:", font=(
-            "Arial, 16"))
-        courseAttendance = tk.Label(master=reportFrame, text=attendancePercentage, font=(
-            "Arial, 16"))
+        courseName = ctk.CTkLabel(
+            master=reportFrame, text=f"{course}:", font=font16)
+        courseAttendance = ctk.CTkLabel(
+            master=reportFrame, text=attendancePercentage, font=font16)
         courseName.grid(row=index, column=0, sticky=tk.E, padx=8)
         courseAttendance.grid(row=index, column=1, sticky=tk.W, padx=8)
 
-    detailsButton = ttk.Button(
+    detailsButton = ctk.CTkButton(
         master=window, text="Download detailed report", command=lambda: downloadReport(id))
-    detailsButton.place(relx=0.15, rely=0.90)
+    detailsButton.place(relx=0.10, rely=0.90)
     window.mainloop()
