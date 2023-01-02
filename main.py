2fca92eb7df94a192f6506f518e67c878b5df012
= <=>
2023-01-02 17:42:18 +0500

    Setup sidebar in windows
    Former-commit-id: aaf11ef9f96b63ba02f0eb6548c2a09adcfae265 [formerly c0b0ad533efd6f3705f027819121266453d5bcc6]
    Former-commit-id: 1a653b161b6b58ecce1cae7b3f5e246fc7a9dcd5
    Former-commit-id: fd5d5a0dfa76e2e49a4949d8cbe17e613b73e1de [formerly 46a24fb1c934758df96ed9342f4598efbcdeb590]
    Former-commit-id: d53c6a2e94a30e50ab3fc5c8f711d65b7673bc1d

--- main.py
+++ main.py
@@ -2,58 +2,26 @@ import tkinter as tk
 import customtkinter as ctk
 from PIL import Image
 
-import add_student
-import attendance
-import student_list
-from getTotalAttendanceReport import getReport
+from sideBar import showSidebar
 
 ctk.set_appearance_mode("System")
 ctk.set_default_color_theme("green")
 
 window = ctk.CTk()
 window.title("AMS")
-window.resizable(width=False, height=False)
-window.geometry('720x480')
+window.geometry('1280x720')
+window.minsize(1280, 720)
 window.iconbitmap('resources/logo.ico')
+window.config(bg='#ffffff')
 
 font = ctk.CTkFont(family="Arial", size=20)
 font16 = ctk.CTkFont(family="Arial", size=16)
 
-titleFrame = ctk.CTkFrame(
-    master=window, bg_color="transparent", fg_color="transparent")
-titleFrame.pack(pady=16)
+showSidebar(window, None)
 
-titleImage = ctk.CTkLabel(
-    titleFrame, image=ctk.CTkImage(light_image=Image.open('resources/logo.png'), dark_image=Image.open('resources/logo.png'), size=(160, 160)), text="")
-titleImage.grid(row=0, column=0, sticky=tk.W)
-title = ctk.CTkLabel(
-    master=titleFrame, text="Attendance Management\nSystem", font=font16, anchor=tk.W, justify='left')
-title.grid(pady=8, row=0, column=1, sticky=tk.E)
+rightFrame = ctk.CTkFrame(
+    master=window, bg_color="transparent", fg_color="#ffffff", width=960)
+rightFrame.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
 
-
-buttonProperties = {
-    'pady': 8,
-    'padx': 26,
-    'column': 0
-}
-
-buttonsFrame = ctk.CTkFrame(master=window, bg_color="transparent")
-buttonsFrame.pack(padx=210, pady=0, fill=tk.X)
-
-getAttendanceReportButton = ctk.CTkButton(
-    master=buttonsFrame, text="Get attendance report", width=250, font=font, command=getReport)
-getAttendanceReportButton.grid(**buttonProperties)
-
-addStudentButton = ctk.CTkButton(
-    master=buttonsFrame, text="Add student", font=font, width=250, command=add_student.main)
-addStudentButton.grid(**buttonProperties)
-
-showAllStudentsButton = ctk.CTkButton(
-    master=buttonsFrame, text="Show All students", width=250, font=font, command=student_list.main)
-showAllStudentsButton.grid(**buttonProperties)
-
-initializeSystem = ctk.CTkButton(
-    master=buttonsFrame, text="Initialize System", width=250, font=font, command=attendance.main)
-initializeSystem.grid(**buttonProperties)
 
 window.mainloop()
---
+++ sideBar.py
@@ -0,0 +1,105 @@
+import tkinter as tk
+import customtkinter as ctk
+from PIL import Image
+
+import add_student
+import attendance
+import student_list
+from getTotalAttendanceReport import getReport
+# from content import showContent
+
+
+def showSidebar(root_window, active_button):
+
+    font = ctk.CTkFont(family="Arial", size=20)
+    font16 = ctk.CTkFont(family="Arial", size=16)
+
+    # clear content of root window
+    for widget in root_window.winfo_children():
+        widget.destroy()
+
+    def onAddStudentButtonPress():
+        active_button = 'addStudentButton'
+        showSidebar(root_window, active_button)
+        # add_student.main(root_window)
+
+    def onShowAllStudentsButtonPress():
+        active_button = 'showAllStudentsButton'
+        showSidebar(root_window, active_button)
+        # student_list.main(root_window)
+
+    def onInitializeSystemButtonPress():
+        active_button = 'initializeSystem'
+        showSidebar(root_window, active_button)
+        # attendance.main(root_window)
+
+    leftFrame = ctk.CTkFrame(master=root_window, bg_color="#f5f5f5", width=320)
+    leftFrame.pack(side=tk.LEFT, fill=tk.Y)
+
+    logoFrame = ctk.CTkFrame(
+        master=leftFrame, bg_color="transparent", fg_color="transparent")
+    logoFrame.pack(padx=24, pady=24, fill=tk.X)
+
+    logoImage = ctk.CTkLabel(
+        logoFrame, image=ctk.CTkImage(light_image=Image.open('resources/header.png'), dark_image=Image.open('resources/header.png'), size=(277, 77)), text="")
+    logoImage.grid(row=0, column=0, sticky=tk.W)
+
+    leftButtonsFrame = ctk.CTkFrame(
+        master=leftFrame, bg_color="transparent", fg_color="transparent")
+    leftButtonsFrame.pack(pady=16, fill=tk.BOTH, expand=True)
+
+    buttonsConfig = {
+        "addStudentButton": {
+            "bg": "#ffffff" if active_button != 'addStudentButton' else "#6FFD9D",
+            "fg": "#000000",
+            "activebackground": "#ffffff" if active_button != 'addStudentButton' else "#62E48C",
+            "activeforeground": "#000000",
+            "highlightthickness": 0,
+            "relief": tk.FLAT,
+            "justify": tk.LEFT,
+            "pady": 16
+        },
+        "showAllStudentsButton": {
+            "bg": "#ffffff" if active_button != 'showAllStudentsButton' else "#6FFD9D",
+            "fg": "#000000",
+            "activebackground": "#ffffff" if active_button != 'showAllStudentsButton' else "#62E48C",
+            "activeforeground": "#000000",
+            "highlightthickness": 0,
+            "relief": tk.FLAT,
+            "justify": tk.LEFT,
+            "pady": 16
+        },
+        "initializeSystem": {
+            "bg": "#ffffff" if active_button != 'initializeSystem' else "#6FFD9D",
+            "fg": "#000000",
+            "activebackground": "#ffffff" if active_button != 'initializeSystem' else "#62E48C",
+            "activeforeground": "#000000",
+            "highlightthickness": 0,
+            "relief": tk.FLAT,
+            "justify": tk.LEFT,
+            "pady": 16
+        },
+
+    }
+
+    addStudentButton = tk.Button(
+        master=leftButtonsFrame, text="Add student", font=font, command=onAddStudentButtonPress, borderwidth=0, **buttonsConfig['addStudentButton'])
+    addStudentButton.pack(fill=tk.X)
+
+    showAllStudentsButton = tk.Button(
+        master=leftButtonsFrame, text="Show All students", font=font, command=onShowAllStudentsButtonPress, borderwidth=0, **buttonsConfig['showAllStudentsButton'])
+    showAllStudentsButton.pack(fill=tk.X)
+
+    initializeSystem = tk.Button(
+        master=leftButtonsFrame, text="Initialize System", font=font, command=onInitializeSystemButtonPress, borderwidth=0, **buttonsConfig['initializeSystem'])
+    initializeSystem.pack(fill=tk.X)
+
+    getAttendanceReportButton = tk.Button(
+        master=leftButtonsFrame, text="Get attendance report", font=font, command=getReport, borderwidth=0, bg="#E5FFEE", fg="#000000", activebackground="#ffffff", activeforeground="#000000", highlightthickness=0, relief=tk.FLAT, justify=tk.LEFT, pady=16)
+    getAttendanceReportButton.pack(
+        fill=tk.X)
+    emptyFrame = ctk.CTkFrame(
+        master=leftButtonsFrame, bg_color="transparent", fg_color="transparent")
+    emptyFrame.pack(fill=tk.Y, expand=True, before=getAttendanceReportButton)
+
+    # showContent(root_window, active_button)
